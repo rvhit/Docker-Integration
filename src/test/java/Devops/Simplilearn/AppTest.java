2251cb4f76d0df83b627a2f96b9eb775588456cf
First Last <rathodrohith2mi@ip-172-31-28-168.ec2.internal>
2021-09-11 01:25:41 +0000

    Added Code and Dockerfile

--- src/test/java/Devops/Simplilearn/AppTest.java
+++ src/test/java/Devops/Simplilearn/AppTest.java
@@ -1,38 +1,12 @@
 package Devops.Simplilearn;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public static void printHello()
+	{
+		System.out.println("Hello World.");
+	}
 }
